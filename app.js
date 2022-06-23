2a0c3b85e392930ce1a4f15466671dd0bc1fc704
code-genier <75497784+code-genier@users.noreply.github.com>
2022-06-23 10:49:15 +0530

    2

--- app.js
+++ app.js
@@ -27,150 +27,150 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", async (req, res) => {
-  userSchema.findOne({ email: req.body.useremail }, (e, foundUser) => {
-    if (e) {
-      console.log(e);
-    } else {
-      if (foundUser) {
-        res.render("areg");
-      }
-    }
-  });
+// app.post("/register", async (req, res) => {
+//   userSchema.findOne({ email: req.body.useremail }, (e, foundUser) => {
+//     if (e) {
+//       console.log(e);
+//     } else {
+//       if (foundUser) {
+//         res.render("areg");
+//       }
+//     }
+//   });
 
-  await userSchema.create({
-    username: req.body.username,
-    email: req.body.useremail,
-    password: req.body.password,
-  });
+//   await userSchema.create({
+//     username: req.body.username,
+//     email: req.body.useremail,
+//     password: req.body.password,
+//   });
 
-  userSchema.findOne({ email: req.body.useremail }, (e, foundUser) => {
-    if (e) {
-      console.log(e);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === req.body.password) {
-          res.render("secrets", {
-            id: foundUser._id,
-            name: req.body.username,
-            userdata: foundUser.datas,
-          });
-        } else {
-          res.render("opps");
-        }
-      } else {
-        res.render("opps");
-      }
-    }
-  });
-});
+//   userSchema.findOne({ email: req.body.useremail }, (e, foundUser) => {
+//     if (e) {
+//       console.log(e);
+//     } else {
+//       if (foundUser) {
+//         if (foundUser.password === req.body.password) {
+//           res.render("secrets", {
+//             id: foundUser._id,
+//             name: req.body.username,
+//             userdata: foundUser.datas,
+//           });
+//         } else {
+//           res.render("opps");
+//         }
+//       } else {
+//         res.render("opps");
+//       }
+//     }
+//   });
+// });
 
-app.post("/login", async (req, res) => {
-  const userEmail = req.body.useremail;
-  const userPassword = req.body.password;
+// app.post("/login", async (req, res) => {
+//   const userEmail = req.body.useremail;
+//   const userPassword = req.body.password;
 
-  userSchema.findOne({ email: userEmail }, (e, foundUser) => {
-    if (e) {
-      console.log(e);
-    } else {
-      if (foundUser) {
-        // console.log(foundUser);
-        if (foundUser.password === userPassword) {
-          res.render("secrets", {
-            id: foundUser._id,
-            name: foundUser.username,
-            userdata: foundUser.datas,
-          });
-        }
-        else{
-          res.render('opps');
-        }
-      }
-      else{
-        res.render('opps');
-      }
-    }
-  });
-});
+//   userSchema.findOne({ email: userEmail }, (e, foundUser) => {
+//     if (e) {
+//       console.log(e);
+//     } else {
+//       if (foundUser) {
+//         // console.log(foundUser);
+//         if (foundUser.password === userPassword) {
+//           res.render("secrets", {
+//             id: foundUser._id,
+//             name: foundUser.username,
+//             userdata: foundUser.datas,
+//           });
+//         }
+//         else{
+//           res.render('opps');
+//         }
+//       }
+//       else{
+//         res.render('opps');
+//       }
+//     }
+//   });
+// });
 
-app.post("/shortUrl", async (req, res) => {
-  const id = req.body.id;
-  //   console.log(id);
-  await userSchema.updateOne(
-    { _id: id },
-    { $push: { datas: { full: req.body.fullUrl } } }
-  );
+// app.post("/shortUrl", async (req, res) => {
+//   const id = req.body.id;
+//   //   console.log(id);
+//   await userSchema.updateOne(
+//     { _id: id },
+//     { $push: { datas: { full: req.body.fullUrl } } }
+//   );
 
-  userSchema.findOne({ _id: id }, (e, foundUser) => {
-    if (e) {
-      console.log(e);
-    } else {
-      if (foundUser) {
-        res.render("secrets", {
-          id: foundUser._id,
-          name: foundUser.username,
-          userdata: foundUser.datas,
-        });
-      }
-    }
-  });
-});
+//   userSchema.findOne({ _id: id }, (e, foundUser) => {
+//     if (e) {
+//       console.log(e);
+//     } else {
+//       if (foundUser) {
+//         res.render("secrets", {
+//           id: foundUser._id,
+//           name: foundUser.username,
+//           userdata: foundUser.datas,
+//         });
+//       }
+//     }
+//   });
+// });
 
-app.post("/delete", async (req, res) => {
-  const deleteItem = req.body.deleteItem;
-  const userid = req.body.userid;
-  //   console.log(deleteItem);
+// app.post("/delete", async (req, res) => {
+//   const deleteItem = req.body.deleteItem;
+//   const userid = req.body.userid;
+//   //   console.log(deleteItem);
 
-  await userSchema.updateOne(
-    { _id: userid },
-    { $pull: { datas: { _id: deleteItem } } }
-  );
+//   await userSchema.updateOne(
+//     { _id: userid },
+//     { $pull: { datas: { _id: deleteItem } } }
+//   );
 
-  userSchema.findOne({ _id: userid }, (e, foundUser) => {
-    if (e) {
-      console.log(e);
-    } else {
-      if (foundUser) {
-        //   console.log(userid);
-        // console.log(foundUser);
-        res.render("secrets", {
-          id: foundUser._id,
-          name: foundUser.username,
-          userdata: foundUser.datas,
-        });
-      }
-    }
-  });
+//   userSchema.findOne({ _id: userid }, (e, foundUser) => {
+//     if (e) {
+//       console.log(e);
+//     } else {
+//       if (foundUser) {
+//         //   console.log(userid);
+//         // console.log(foundUser);
+//         res.render("secrets", {
+//           id: foundUser._id,
+//           name: foundUser.username,
+//           userdata: foundUser.datas,
+//         });
+//       }
+//     }
+//   });
 
-  //   console.log(userid);
-  //   console.log(deleteItem);
-});
+//   //   console.log(userid);
+//   //   console.log(deleteItem);
+// });
 
-app.get("/:shortUrl", async (req, res) => {
-  const need = req.params.shortUrl;
-  var partsArray = need.split("?");
+// app.get("/:shortUrl", async (req, res) => {
+//   const need = req.params.shortUrl;
+//   var partsArray = need.split("?");
 
-  const id = partsArray[0];
-  const needUrl = partsArray[1];
-  let url;
+//   const id = partsArray[0];
+//   const needUrl = partsArray[1];
+//   let url;
 
-  const needUser = await userSchema.findOne({
-    _id: id,
-  });
-  if (needUser == null) {
-    console.log("not");
-    return res.render("opps");
-  }
-  await needUser.datas.forEach((itr) => {
-    if (itr.shorts === needUrl) {
-      url = itr.full;
-      itr.clicks++;
-      needUser.save();
-    }
-  });
+//   const needUser = await userSchema.findOne({
+//     _id: id,
+//   });
+//   if (needUser == null) {
+//     console.log("not");
+//     return res.render("opps");
+//   }
+//   await needUser.datas.forEach((itr) => {
+//     if (itr.shorts === needUrl) {
+//       url = itr.full;
+//       itr.clicks++;
+//       needUser.save();
+//     }
+//   });
 
-  res.redirect(url);
-});
+//   res.redirect(url);
+// });
 
 
 let port = process.env.PORT;
